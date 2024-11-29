276b0d8671d8e16bc615561ff7174895b03da620
SriJyestham <sriindram18@gmail.com>
2024-11-29 03:31:09 -0500

    #tuples # practise

---
+++ practise/gradeTuple.py
@@ -0,0 +1,15 @@
+
+
+tup1 = ("C","D","A","A","B","B","A")
+
+print(tup1)
+
+print(tup1.count("A"))
+
+list1 = ["C","D","A","A","B","B","A"]
+
+list1.sort()
+
+#print(list1.sort())  # does not work as sort() does not
+					  # return anything
+print(list1)
